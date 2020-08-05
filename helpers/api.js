dfae89868e604eae2a4797cf41977a87c6f6e405
bineetNaidu <bineet.s.naidu@gmail.com>
2020-08-05 10:45:19 +0530

    -- add GET / PUT / DELETE route to place api

--- helpers/api.js
+++ helpers/api.js
@@ -26,4 +26,47 @@ module.exports = {
             res.redirect("/");
         }
     },
+    getThePlace: async (req, res, next) => {
+        try {
+            let place = await Place.findOne({ _id: req.params.placeID });
+            if (!place) {
+                return res.send("SOMETHING WENT WRONG WITH THE API");
+            }
+            await res.json(place);
+        } catch (error) {
+            console.log(error);
+            res.redirect("/");
+        }
+    },
+    updateThePlace: async (req, res, next) => {
+        try {
+            let place = await Place.findOne({ _id: req.params.placeID });
+            if (!place) {
+                return res.send("SOMETHING WENT WRONG WITH THE API");
+            }
+            if (req.body.image) place.image = req.body.image;
+            if (req.body.location) place.location = req.body.location;
+            if (req.body.price) place.price = req.body.price;
+            if (req.body.duration) place.duration = req.body.duration;
+            place.save();
+            res.json(place);
+        } catch (error) {
+            console.log(error);
+            res.redirect("/");
+        }
+    },
+    deleteThePlace: async (req, res, next) => {
+        try {
+            Place.remove({ _id: req.params.placeID })
+                .then(function () {
+                    res.json({ message: "Removed the Place" });
+                })
+                .catch(function (err) {
+                    res.send(err);
+                });
+        } catch (error) {
+            console.log(error);
+            res.redirect("/");
+        }
+    },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { getAllPlaces, createPlace } = require("../helpers/api");
+const {
+    getAllPlaces,
+    createPlace,
+    getThePlace,
+    updateThePlace,
+    deleteThePlace,
+} = require("../helpers/api");
 
 // route --- /api/...
 router.route("/places").get(getAllPlaces).post(createPlace);
+router
+    .route("/places/:placeID")
+    .get(getThePlace)
+    .put(updateThePlace)
+    .delete(deleteThePlace);
 
 module.exports = router;
