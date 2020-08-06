ce4acd7ec8776e95262029bf758c2cb3a1c27694
bineetNaidu <bineet.s.naidu@gmail.com>
2020-08-06 17:37:12 +0530

    -- add the places the components -- add the styles -- add function to fetch the places

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,3 +4,33 @@ $(".search-box").on("click", "div.five button.btn-search", (event) => {
     let $input = $("div.five input");
     $input.focus();
 });
+
+$(document).ready(function () {
+    $.getJSON("/api/places")
+        .then(addPlaces)
+        .catch((error) => console.log(error));
+});
+
+function addPlaces(places) {
+    for (const place of places) {
+        let element = `
+    <div class="place__box" style="background-image: url('${place.image[0]}");' data-id="${place._id}">
+            <div class="place__details">
+                <h5>${place.location}</h5>
+                <div class="meta-data">
+                    <p>
+                        <i class="fas fa-briefcase"></i>
+                        ${place.duration} days
+                    </p>
+                    <p>
+                        <i class="fas fa-map-pin"></i>
+                        ${place.price}Km
+                    </p>
+                </div>
+            </div>
+    </div>
+    `;
+
+        $(".places .places__card").append(element);
+    }
+}
